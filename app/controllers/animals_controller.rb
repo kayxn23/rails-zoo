cc41ffbc5de717efdc8f855c815cf380ff64f72e
Kay <kayxn23@gmail.com>
2018-09-29 13:44:33 -0400

    added edit method to controller so that the edit form saves(modifies the database)

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -4,7 +4,7 @@ class AnimalsController < ApplicationController
   end
 
   def show
-    id = params[:id]
+    id = params[:id].to_i
     @animal = Animal.find_by(id: id)
   end
 
@@ -20,12 +20,27 @@ class AnimalsController < ApplicationController
     else
       render :new
     end
-
   end
 
+  def destroy
+    animal = Animal.find_by(id: params[:id].to_i)
+    animal.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @animal = Animal.find_by(id: params[:id].to_i)
+  end
+
+  def update
+    @animal = Animal.find_by(id: params[:id].to_i)
+    @animal.update(animal_params)
+
+    redirect_to animal_path
+  end
 private
   def animal_params
-    return params.require(:animal).permit(:name, :species, :age)
-  end 
+    return params.require(:animal).permit(:name, :species, :age, :image_url)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root "animals#index"
 
-  resources :animals, only: [:index, :show, :create, :new]
+  resources :animals
 
   get '/notfound', to: 'animals#notfound'
+
+  #rails knows :create is  a post by default
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
